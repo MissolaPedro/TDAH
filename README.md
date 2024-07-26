1f9eda9bb400a4f5adb64e696373775bb499f5fd
Test <Test@gmail.com>
2024-07-26 11:17:55 -0300

    Projeto atualizado
    Co-authored-by: heitorsinistro <heitorsinistro@users.noreply.github.com>

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Este um é um repositorio dedicado a armazenar os arquivos do projeto tcc.
 
 1. git config --global user.email "externalconnections.pedro@gmail.com"
 2. git config --global user.name"ExternalPedro"
-3. npm install axios cookie-parser ejs express express-ejs-layouts firebase
-4. npm install autoprefixer browser-sync concurrently postcss sass tailwindcss --save-dev
+3. npm install cookie-parser ejs express express-ejs-layouts express-session firebase
+4. npm install autoprefixer concurrently postcss sass tailwindcss --save-dev
--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,8 @@ const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 
-// Inicialização do Express
 const app = express();
 
-// Configurações do cookie de sessão para uso seguro
 app.use(session({
   secret: '2b0589e317f0ed915feacb2865eec8ea2d5c077bfb08f1e01e820451eb67afdf1be7367733aeba16c1e6e46837cdcf848a27097ae2a6d324bb7b0e8ac4535c1e',
   resave: false,
@@ -20,21 +18,17 @@ app.use(session({
   }
 }));
 
-// Middleware para parsear o corpo das requisições e cookies
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Configurações do Express, EJS e layouts
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, '..', '..', 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
-// Importação e utilização de rotas
 const routeEJS = require('../../src/routes/routeEJS.js');
 routeEJS(app);
 
-// Configuração do servidor HTTP
 const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
