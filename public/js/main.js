bb3a21bcdfb784f1a4c096f76bbf335b3676ed8e
Jhouwtromund0 <jhonataadriano000@gmail.com>
2024-08-22 23:33:40 -0300

    corigindo

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// Importações de módulos
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+  secret: '2b0589e317f0ed915feacb2865eec8ea2d5c077bfb08f1e01e820451eb67afdf1be7367733aeba16c1e6e46837cdcf848a27097ae2a6d324bb7b0e8ac4535c1e',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false, // Defina como false se não estiver usando HTTPS
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 dias
+  }
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, '..', '..', 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+const routeGet = require('../../src/routes/routeGet.js');
+routeGet(app);
+
+const routePost = require('../../src/routes/routePost.js');
+routePost(app);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
---
+++ src/routes/routeEJS.js
@@ -0,0 +1,206 @@
+const { registrarUsuario } = require("../firebase/functions/register");
+const { signInUser } = require("../firebase/functions/login");
+const { signOutUser } = require("../firebase/functions/signout");
+const {
+	sendPasswordResetEmailFirebase,
+} = require("../firebase/functions/resetpassword");
+const dns = require("dns").promises;
+
+function routeEJS(app) {
+	app.get("/", (req, res) => {
+		let successMessage = req.query.loginSuccess
+			? "Login realizado com sucesso!"
+			: null;
+		let registerSuccessMessage = req.query.registerSuccess
+			? "Registro realizado com sucesso!"
+			: null;
+		res.render("index", {
+			title: "Projeto TDAH",
+			query: req.query,
+			successMessage: successMessage,
+			registerSuccessMessage: registerSuccessMessage,
+		});
+	});
+
+	app.get("/auth/status", (req, res) => {
+		try {
+			const isLoggedIn = req.cookies.loggedIn;
+			res.json({ loggedIn: !!isLoggedIn });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	});
+
+	app.get("/login", (req, res) => {
+		res.render("partials/form-login", {
+			title: "Login",
+			errorMessage: null,
+			successMessage: null,
+		});
+	});
+
+	app.get("/resetpassword", (req, res) => {
+		res.render("partials/form-reset", {
+			title: "Resetar a senha",
+			errorMessage: null,
+			successMessage: null,
+		});
+	});
+
+	app.get("/register", (req, res) => {
+		res.render("partials/form-register", {
+			title: "Registre-se",
+			errorMessage: null,
+		});
+	});
+	app.post("/login", async (req, res) => {
+		const { loginemail, loginpassword, loginrememberMe } = req.body;
+
+		if (!loginemail) {
+			return res.render("partials/form-login", {
+				title: "Login",
+				errorMessage: "O email é obrigatório.",
+				successMessage: null,
+			});
+		}
+		if (!loginpassword) {
+			return res.render("partials/form-login", {
+				title: "Login",
+				errorMessage: "A senha é obrigatória.",
+				successMessage: null,
+			});
+		}
+
+		const emailRegex =
+			/^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
+		if (!emailRegex.test(loginemail)) {
+			return res.render("partials/form-login", {
+				title: "Login",
+				errorMessage: "Formato de email inválido.",
+				successMessage: null,
+			});
+		}
+
+		// Extrai o domínio do email
+		const domain = loginemail.split("@")[1];
+		try {
+			await dns.resolveMx(domain); // Verifica se o domínio tem registros MX (Mail Exchange)
+		} catch (error) {
+			return res.render("partials/form-login", {
+				title: "Login",
+				errorMessage: "Domínio de email não existe.",
+				successMessage: null,
+			});
+		}
+
+		if (loginpassword.length < 6) {
+			return res.render("partials/form-login", {
+				title: "Login",
+				errorMessage: "Senha deve ter pelo menos 6 caracteres.",
+				successMessage: null,
+			});
+		}
+
+		signInUser(
+			loginemail,
+			loginpassword,
+			loginrememberMe === "true",
+			req,
+			(error, user) => {
+				if (error) {
+					return res.render("partials/form-login", {
+						title: "Login",
+						errorMessage:
+							"Falha no login. Verifique seu email e senha e tente novamente.",
+						successMessage: null,
+					});
+				}
+				// Define o cookie com a persistência baseada em "rememberMe"
+				const cookieOptions =
+				loginrememberMe === "true"
+						? { maxAge: 900000, httpOnly: true }
+						: { httpOnly: true, maxAge: null };
+				res.cookie("loggedIn", true, cookieOptions);
+				res.redirect("/?loginSuccess=true");
+			}
+		);
+	});
+
+	app.post("/register", (req, res) => {
+		const { registeremail, registerpassword } = req.body;
+		registrarUsuario(registeremail, registerpassword, (error, user) => {
+			if (error) {
+				return res.render("partials/form-register", {
+					errorMessage: "Falha no registro. Tente novamente.",
+				});
+			}
+			console.log("Usuário registrado com sucesso");
+			res.cookie("loggedIn", true, { maxAge: 900000, httpOnly: true });
+			res.redirect("/?registerSuccess=true");
+		});
+	});
+
+	app.get("/logout", (req, res) => {
+		signOutUser(error => {
+			if (error) {
+				console.error("Erro ao deslogar:", error);
+				res.redirect("/");
+			} else {
+				console.log("Usuário deslogado com sucesso");
+				res.clearCookie("loggedIn");
+				res.redirect("/login");
+			}
+		});
+	});
+
+	// Rota POST para redefinição de senha
+
+	app.post("/resetpassword", async (req, res) => {
+		const { email } = req.body;
+		if (!email) {
+			return res.render("partials/form-reset", {
+				title: "Resetar a senha",
+				errorMessage: "O email é obrigatório.",
+				successMessage: null,
+			});
+		}
+		const emailRegex =
+			/^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
+		if (!emailRegex.test(email)) {
+			return res.render("partials/form-reset", {
+				title: "Resetar a senha",
+				errorMessage: "Formato de email inválido.",
+				successMessage: null,
+			});
+		}
+		// Extrai o domínio do email
+		const domain = email.split("@")[1];
+		try {
+			await dns.resolveMx(domain); // Verifica se o domínio tem registros MX (Mail Exchange)
+		} catch (error) {
+			return res.render("partials/form-reset", {
+				title: "Resetar a senha",
+				errorMessage: "Domínio de email não existe.",
+				successMessage: null,
+			});
+		}
+		sendPasswordResetEmailFirebase(email, (error, message) => {
+			if (error) {
+				res.render("partials/form-reset", {
+					title: "Resetar a senha",
+					errorMessage: error,
+					successMessage: null,
+				});
+			} else {
+				res.render("partials/form-reset", {
+					title: "Resetar a senha",
+					errorMessage: null,
+					successMessage: message,
+				});
+			}
+		});
+	});
+}
+
+module.exports = routeEJS;
