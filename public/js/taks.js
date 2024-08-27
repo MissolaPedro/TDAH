781c12dfa181823f2a88ae584faf5c8b5f6b4c81
heitorsinistro <heitorcabral.cartoon@gmail.com>
2024-08-27 10:33:17 -0300

    finalpart-1

--- public/js/taks.js
+++ public/js/taks.js
@@ -1,42 +1,5 @@
-// Função para validar o formato de horário no formato HH:MM
-function validateTime(timeString) {
-    const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
-    return regex.test(timeString);
+const button = document.getElementById('savebutton');
+
+button.addEventListener('click', function){
+    alert('mnaada')
 }
-
-// Função para validar os campos do formulário
-function validateForm() {
-    const startTimeInput = document.getElementById('horacomeco').value;
-    const endTimeInput = document.getElementById('horatermino').value;
-
-    let isValid = true;
-    let errorMessage = '';
-    
-    if (!validateTime(startTimeInput)) {
-        errorMessage += 'Horário de início inválido. O formato correto é HH:MM.\n';
-        isValid = false;
-    }
-
-    if (!validateTime(endTimeInput)) {
-        errorMessage += 'Horário de término inválido. O formato correto é HH:MM.\n';
-        isValid = false;
-    }
-
-    if (!isValid) {
-        alert(errorMessage);
-    }
-
-    return isValid;
-}
-
-// Adiciona um ouvinte de evento para validação do formulário ao enviar
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('myForm');
-    if (form) {
-        form.addEventListener('submit', (event) => {
-            if (!validateForm()) {
-                event.preventDefault(); // Impede o envio do formulário se a validação falhar
-            }
-        });
-    }
-});
